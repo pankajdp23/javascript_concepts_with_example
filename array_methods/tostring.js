88f5bdcc5b91608421d2baea65d3a59ba7fea0a8
pankajdp23 <pankajdp23@gmail.com>
2023-05-19 13:25:38 +0530

    minor code formatted

--- array_methods/tostring.js
+++ array_methods/tostring.js
@@ -8,7 +8,9 @@ The toString() method does not change the original array.
 # Note:
 Every JavaScript object has a toString() method.
 
-The toString() method is used internally by JavaScript when an object needs to be displayed as a text (like in HTML), or when an object needs to be used as a string.
+The toString() method is used internally by JavaScript
+when an object needs to be displayed as a text (like in HTML), 
+or when an object needs to be used as a string.
 
 Normally, you will not use it in your own code.
 
